99bb8c22e865a31c8beaa30b5d1e8f2923deec12
Sam Taylor <samjt775@gmail.com>
2025-01-23 09:43:13 +1000

    fix: Openssl compile, set for arm32 and install path to correct location

--- scripts/cmake/openssl.cmake
+++ scripts/cmake/openssl.cmake
@@ -9,15 +9,20 @@ ExternalProject_Add(
     GIT_REPOSITORY https://github.com/openssl/openssl.git
     GIT_TAG master
     GIT_SHALLOW 1
-    CMAKE_ARGS
-        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
-        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/external/install
     PREFIX ${CMAKE_BINARY_DIR}/external/openssl/prefix
     TMP_DIR ${CMAKE_BINARY_DIR}/external/openssl/tmp
     STAMP_DIR ${CMAKE_BINARY_DIR}/external/openssl/stamp
     DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/external/openssl/download
     SOURCE_DIR ${CMAKE_BINARY_DIR}/external/openssl/src
-    BINARY_DIR ${CMAKE_BINARY_DIR}/external/openssl/build
+    BUILD_IN_SOURCE TRUE
+    CONFIGURE_COMMAND 
+        ./Configure 
+        linux-generic32 
+        --prefix=${CMAKE_BINARY_DIR}/external/install 
+        --openssldir=${CMAKE_BINARY_DIR}/external/install/ssl 
+        CFLAGS="-I${CMAKE_BINARY_DIR}/external/install/include" 
+        LDFLAGS="-L${CMAKE_BINARY_DIR}/external/install/lib -Wl,-rpath,${CMAKE_BINARY_DIR}/external/install/lib"
+    INSTALL_COMMAND make install
     UPDATE_DISCONNECTED 1
     BUILD_ALWAYS 0
 )
