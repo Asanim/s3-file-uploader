7e3ce8e22d5225364be94f9be284bddb3faae89c
Sam Taylor <samjt775@gmail.com>
2025-05-08 15:51:22 +1000

    fix: curl

--- scripts/cmake/curl.cmake
+++ scripts/cmake/curl.cmake
@@ -1,34 +1,24 @@
 include(ExternalProject)
 
-# curl version
-set(CURL_VERSION "7.88.1" CACHE STRING "curl version")
+message(STATUS "Added curl to external submodules")
+# ------------------------------------------------------------------------------
+# curl
+# ------------------------------------------------------------------------------
 
-# Get OS and architecture information
-if(CMAKE_CROSSCOMPILING)
-    set(CURL_TARGET_SYSTEM ${CMAKE_SYSTEM_NAME})
-else()
-    set(CURL_TARGET_SYSTEM ${CMAKE_HOST_SYSTEM_NAME})
-endif()
-
-# Set up paths
-set(CURL_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/curl)
-set(CURL_INSTALL_DIR ${CURL_PREFIX}/install)
-set(CURL_INCLUDE_DIR ${CURL_INSTALL_DIR}/include)
-set(CURL_LIB_DIR ${CURL_INSTALL_DIR}/lib)
-
-# Set up library names with platform-specific extensions
-if(CURL_TARGET_SYSTEM STREQUAL "Windows")
-    set(CURL_STATIC_LIB "${CURL_LIB_DIR}/libcurl.lib")
-    set(CURL_IMPORT_LIB "${CURL_LIB_DIR}/libcurl_imp.lib")
-    set(CURL_SHARED_LIB "${CURL_LIB_DIR}/libcurl.dll")
-else()
-    set(CURL_STATIC_LIB "${CURL_LIB_DIR}/libcurl.a")
-    set(CURL_SHARED_LIB "${CURL_LIB_DIR}/libcurl${CMAKE_SHARED_LIBRARY_SUFFIX}")
-endif()
-
-# Build options for curl
-set(CURL_CMAKE_ARGS
-    -DCMAKE_INSTALL_PREFIX=${CURL_INSTALL_DIR}
+ExternalProject_Add(
+    libcurl
+    URL https://curl.se/download/curl-${CURL_VERSION}.tar.gz
+    PREFIX ${CMAKE_BINARY_DIR}/external/curl/prefix
+    TMP_DIR ${CMAKE_BINARY_DIR}/external/curl/tmp
+    STAMP_DIR ${CMAKE_BINARY_DIR}/external/curl/stamp
+    DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/external/curl/download
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/external/curl/src
+    BINARY_DIR ${CMAKE_BINARY_DIR}/external/curl/build
+    CMAKE_ARGS
+    -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+    -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+    -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/external/install
     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
     -DBUILD_SHARED_LIBS=OFF
     -DBUILD_CURL_EXE=OFF
@@ -44,38 +34,8 @@ set(CURL_CMAKE_ARGS
     -DOPENSSL_CRYPTO_LIBRARY=${CMAKE_BINARY_DIR}/external/install/lib/libcrypto.so
     -DOPENSSL_SSL_LIBRARY=${CMAKE_BINARY_DIR}/external/install/lib/libssl.so
 
-)
-
-# Add toolchain file if crosscompiling
-if(CMAKE_TOOLCHAIN_FILE)
-    list(APPEND CURL_CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE})
-endif()
-
-# Additional compiler flags
-if(CMAKE_C_FLAGS)
-    list(APPEND CURL_CMAKE_ARGS -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS})
-endif()
-
-if(CMAKE_CXX_FLAGS)
-    list(APPEND CURL_CMAKE_ARGS -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS})
-endif()
-
-# Define the external project
-ExternalProject_Add(libcurl
-    URL https://curl.se/download/curl-${CURL_VERSION}.tar.gz
-    PREFIX ${CURL_PREFIX}
-    CMAKE_ARGS ${CURL_CMAKE_ARGS}
     BUILD_BYPRODUCTS ${CURL_STATIC_LIB}
+    UPDATE_DISCONNECTED 1
+    BUILD_ALWAYS 0
 )
 
-# Create imported library
-add_library(curl::libcurl STATIC IMPORTED GLOBAL)
-set_target_properties(curl::libcurl PROPERTIES
-    IMPORTED_LOCATION ${CURL_STATIC_LIB}
-    INTERFACE_INCLUDE_DIRECTORIES ${CURL_INCLUDE_DIR}
-)
-add_dependencies(curl::libcurl libcurl)
-
-# Export variables for the parent scope
-set(CURL_INCLUDE_DIRS ${CURL_INCLUDE_DIR} CACHE PATH "Path to curl include directory")
-set(CURL_LIBRARIES ${CURL_STATIC_LIB} CACHE FILEPATH "Path to curl library")
