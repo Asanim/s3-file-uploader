d875ef4c27152e45a1fa48246234a7a0805c68f3
Sam Taylor <samjt775@gmail.com>
2025-05-10 13:46:01 +1000

    fix: add toolchain downloads from arm developer site

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-curl -LO https://storage.googleapis.com/mirror.tensorflow.org/developer.arm.com/media/Files/downloads/gnu-a/8.3-2019.03/binrel/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz
+# curl -LO https://storage.googleapis.com/mirror.tensorflow.org/developer.arm.com/media/Files/downloads/gnu-a/8.3-2019.03/binrel/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz
+
+# Download and extract the ARM toolchain to the build directory
+
+mkdir -p $SCRIPT_PATH/../build
+cd $SCRIPT_PATH/../build
+
 mkdir -p ${HOME}/toolchains
-tar xvf gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz -C ${HOME}/toolchains
+
+echo "Downloading and extracting the ARM toolchain..."
+curl -LO https://developer.arm.com/-/media/Files/downloads/gnu/14.2.rel1/binrel/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-linux-gnueabihf.tar.xz
+tar xvf arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-linux-gnueabihf.tar.xz -C ${HOME}/toolchains
+
+echo "Downloading and extracting the ARM64 toolchain..."
+curl -LO https://developer.arm.com/-/media/Files/downloads/gnu/14.2.rel1/binrel/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
+tar xvf arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz -C ${HOME}/toolchains
+
