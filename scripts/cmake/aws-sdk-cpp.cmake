72548b9678be987650136b1b5aec94708b8b8668
Sam Taylor <samjt775@gmail.com>
2025-05-10 11:43:08 +1000

    set aws sdk to use crypto openssl

--- scripts/cmake/aws-sdk-cpp.cmake
+++ scripts/cmake/aws-sdk-cpp.cmake
@@ -13,9 +13,10 @@ ExternalProject_Add(aws-sdk-cpp
     -DAUTORUN_UNIT_TESTS=OFF
     -DBUILD_ONLY=s3
     -DBUILD_SHARED_LIBS=ON
-    -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/external/install
+    -DCMAKE_INSTALL_PREFIX=
     -DCMAKE_PREFIX_PATH=${CMAKE_BINARY_DIR}/external/install
     -DCMAKE_BUILD_TYPE=Release
+    -DUSE_OPENSSL=ON
 
     # Zlib settings
     -DZLIB_LIBRARY=${CMAKE_BINARY_DIR}/external/install/lib/libz.so
@@ -24,6 +25,14 @@ ExternalProject_Add(aws-sdk-cpp
     # libcurl settings
     -DCURL_LIBRARY=${CMAKE_BINARY_DIR}/external/install/lib/libcurl.so
     -DCURL_INCLUDE_DIR=${CMAKE_BINARY_DIR}/external/install/include/
+
+    # libcrypto settings
+    -Dcrypto_INCLUDE_DIR=${CMAKE_BINARY_DIR}/external/install/include
+    -Dcrypto_LIBRARY=${CMAKE_BINARY_DIR}/external/install/lib/libcrypto.so
+    -Dcrypto_STATIC_LIBRARY=${CMAKE_BINARY_DIR}/external/install/lib/libcrypto.a
+    -Dcrypto_SHARED_LIBRARY=${CMAKE_BINARY_DIR}/external/install/lib/libcrypto.so
+    -DOPENSSL_CRYPTO_LIBRARY=${CMAKE_BINARY_DIR}/external/install/lib/libcrypto.so
+    -DOPENSSL_INCLUDE_DIR=${CMAKE_BINARY_DIR}/external/install/include
 
     PREFIX ${CMAKE_BINARY_DIR}/external/aws-sdk-cpp/prefix
     TMP_DIR ${CMAKE_BINARY_DIR}/external/aws-sdk-cpp/tmp
