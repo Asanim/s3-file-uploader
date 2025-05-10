0c2f832677a5c7af737c343a22bef82ed27604db
Sam Taylor <samjt775@gmail.com>
2025-05-10 15:32:19 +1000

    Add release build step, fix cmake

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# curl -LO https://storage.googleapis.com/mirror.tensorflow.org/developer.arm.com/media/Files/downloads/gnu-a/8.3-2019.03/binrel/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf.tar.xz
+    sudo apt-get update
+    sudo apt-get install -y build-essential cmake libcurl4-openssl-dev libssl-dev uuid-dev zlib1g-dev
 
 # Download and extract the ARM toolchain to the build directory
 
@@ -18,3 +19,5 @@ echo "Downloading and extracting the ARM64 toolchain..."
 curl -LO https://developer.arm.com/-/media/Files/downloads/gnu/14.2.rel1/binrel/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
 tar xvf arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz -C ${HOME}/toolchains
 
+# Install gdk - greengrass development kit
+pip3 install -U git+https://github.com/aws-greengrass/aws-greengrass-gdk-cli.git@v1.6.2
