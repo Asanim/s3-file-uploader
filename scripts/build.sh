e3b86d650afbb36ce71fde40fc5e25784bc62f84
Sam Taylor <samjt775@gmail.com>
2025-05-10 16:13:39 +1000

    feat: add scripts to deploy this project as a greengrass component

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 # Check if the number of command-line arguments is not equal to 2
 if [ $# -ne 2 ]; then
   echo 1>&2 "Usage: $0 COMPONENT-NAME COMPONENT-VERSION"
-  exit 3
+  exit 1
 fi
 
 # Assign command-line arguments to variables
@@ -13,19 +13,10 @@ VERSION=$2
 # Get the path of the script
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Remove any existing build artifacts
-rm -rf $SCRIPT_PATH/../build/greengrass-build
-rm -rf $SCRIPT_PATH/../build/s3-file-uploader_*.deb
-
 # Recompile the component
 cd $SCRIPT_PATH/../build
-cmake .. && make -j$(nproc-1) && cpack
-
-# Create directories if they don't exist
-mkdir -p $SCRIPT_PATH/../build/greengrass-build/recipes $SCRIPT_PATH/../build/greengrass-build/artifacts
-
-# Copy recipe to greengrass-build
-cp recipe.json $SCRIPT_PATH/../build/greengrass-build/recipes
+cmake .. && make -j$(nproc -1) && cpack
 
 # Copy the renamed archive to greengrass-build
+mkdir -p $SCRIPT_PATH/../build/greengrass-build/artifacts/$COMPONENT_NAME/$VERSION/
 cp $SCRIPT_PATH/../build/s3-file-uploader_*.deb $SCRIPT_PATH/../build/greengrass-build/artifacts/$COMPONENT_NAME/$VERSION/
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,8 @@ if [ $# -ne 1 ]; then
     echo "Usage: $0 COMPONENT_VERSION"
     exit 1
 fi
+# Get the path of the script
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Define environment variables
 export COMPONENT_VERSION="$1"
@@ -14,22 +16,29 @@ export BUCKET_NAME="greengrass-artifacts"
 export BUCKET_NAME_FULL="greengrass-artifacts-${AWS_REGION}-${AWS_ACCOUNT_ID}"
 export COMPONENT_NAME="com.example.s3-file-uploader"
 export COMPONENT_AUTHOR="Asanim"
+export BUILD_SCRIPT_PATH="$SCRIPT_PATH/build.sh"
 
-SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Remove any existing build artifacts
+rm -rf $SCRIPT_PATH/../build/greengrass-build
+rm -rf $SCRIPT_PATH/../build/s3-file-uploader_*.deb
+
+# Create directories if they don't exist
+mkdir -p $SCRIPT_PATH/../build/greengrass-build/recipes $SCRIPT_PATH/../build/greengrass-build/artifacts
 
 cd $SCRIPT_PATH/../build
 
 # Create 'recipe.json' using environment variables
 envsubst < "$SCRIPT_PATH/templates/recipe.json.template" > "recipe.json"
-cat recipe.json
 
 # Create 'gdk-config.json' using environment variables
 envsubst < "$SCRIPT_PATH/templates/gdk-config.json.template" > "gdk-config.json"
-cat gdk-config.json
 
 # Build and publish the component
 echo "Building the component"
 gdk component build
+
+# Hotfix
+cp recipe.json $SCRIPT_PATH/../build/greengrass-build/recipes/recipe.json
 
 echo "Publish the component"
 gdk component publish
